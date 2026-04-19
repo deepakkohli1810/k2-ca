78940de3b4bb9fdd0ef39cb5bd63ee492362e88b
deepakkohli1810 <deepakkohli1810@gmail.com>
2026-04-19 09:48:25 +0530

    added all the necessary files

---
+++ src/components/home/HomeHeroText.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomeHeroText = () => {
+    return (
+        <div className='font-[font1] pt-4'>
+
+            <div className='text-[9.5vw] uppercase leading-[8.5vw] text-center'>L'étincelle </div>
+            <div className='text-[9.5vw] uppercase leading-[8.5vw] text-center'>quigénère</div>
+            <div className='text-[9.5vw] uppercase leading-[8.5vw] text-center'>la créativité</div>
+        </div>
+    )
+}
+
+export default HomeHeroText
