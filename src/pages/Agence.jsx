aab6f09f7675e931d9f03774fcd107f5a2d3d7a0
deepakkohli1810 <deepakkohli1810@gmail.com>
2026-04-22 22:36:16 +0530

    Code need to fix the agence page , there is some internal  issue of the vs code

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -10,7 +10,7 @@ const Agence = () => {
  const imgRef = useRef(null);
 
  const ImageArray = [
-  ' https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64 ',
+  'https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64',
   'https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991',
   'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
   'https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6',
@@ -24,10 +24,10 @@ const Agence = () => {
   gsap.to(imageDivRef.current, {
    scrollTrigger: {
     trigger: imageDivRef.current,
-    // markers: true, its helpful for visualizing area you want to trigger the animation.
-    start: 'top 19.2%',
-    end: 'top -80%',
-    scrub: 1,
+    markers: true, 
+    start: 'top 20%',
+    end: 'top -70%',
+    scrub: 1, //smooth scrubbing with 1s easing 
     pin: true, // it is used to fix the image div in place while scrolling, creating a parallax effect
     pinSpacing:true, 
     pinReparent:true, 
@@ -52,48 +52,37 @@ const Agence = () => {
  });
 
  return (
-  <div>
-   {/* Section 1  */}
-   <div className="section1 py-1">
-    {/* Image Div */}
-    <div
-     ref={imageDivRef}
-     className="absolute w-[15vw] overflow-hidden rounded-4xl  h-[20vw]  z "
-    >
-     <img
-      ref={imgRef}
-      className="w-full h-full object-cover"
-      src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
-      alt=""
-     />
-    </div>
 
-    {/* Text div  */}
-    <div className="relative">
-     <div className="font-[font2] mt-[53vh]">
-      <h1 className="text-[20vw] text-center leading-[18vw] uppercase ">
-       Soixan7e <br />
-       Douze
-      </h1>
+<div className='parent bg-white text-black'>
+    <div id='page1' className='py-1'>
+        {/* //Image Div  */}
+     <div ref={imageDivRef}  className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw] 
+      left-[30vw] top-30 bg-blue-900'>
+       <img className='h-full object-cover w-full' ref={imgRef} 
+       src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+     </div>
+     {/* Text Div */}
+     <div className='relative font-[font2]'>
+     {/* <div className='mt-[55vh]'>
+    <h1 className='text-[20vw] text-center uppercase leading-[18vw]'> soixan7e <br /> douze</h1>
+     </div> */}
+     {/* Paragraph Div */}
+     <div className='pl-[40%] mt-20'>
+        <p className='text-6xl'>Notre curiosité nourrit notre créativité. 
+            On reste humbles et on dit non aux gros egos, même le vôtre. 
+            Une marque est vivante. Elle a des valeurs, une personnalité,
+             une histoire. Si on oublie ça, on peut faire de bons chiffres à court 
+             terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner 
+            de la perspective, pour bâtir des marques influentes.</p>
      </div>
 
-     <div className="pl-[40%] font-[font2]">
-      <p className="text-5xl  ">
-       &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-       &nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On
-       reste humbles et on dit non aux gros egos, même le vôtre. Une marque est
-       vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie
-       ça, on peut faire de bons chiffres à court terme, mais on la tue à long
-       terme. C’est pour ça qu’on s’engage à donner de la perspective, pour
-       bâtir des marques influentes.
-      </p>
      </div>
     </div>
-   </div>
 
-   {/* Section 2  */}
-   <div className="section2 h-screen "></div>
-  </div>
+    <div id='page2' className='h-screen'>
+
+    </div>
+</div>
  );
 };
 
