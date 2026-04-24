333e307877f60a286f42962edf656a3321bd88bb
deepakkohli1810 <deepakkohli1810@gmail.com>
2026-04-24 22:09:35 +0530

    Navbar feature made and differnt types of animation included

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -2,7 +2,121 @@ import React from 'react'
 
 const FullScreenNav = () => {
   return (
-    <div>FullScreenNav</div>
+    <div id='fullScreenNav' className=' h-screen w-full py-40 absolute black'>
+        <div id='all-links' className=''>
+            <div className='link relative border-t border-white'>
+
+                <h1 className=' font-[font2] leading-[0.8] text-center pt-10 text-[8vw] uppercase'>Projets</h1>
+                <div className='moveLink absolute flex top-0 bg-[#cfed52] text-black'>
+
+                   <div className=' moveX flex items-center'>
+                     <h2 className='whitespace-nowrap  font-[font2] leading-[0.8] text-center pt-10 text-[8vw] 
+                     uppercase'>pour tour voir</h2>
+                    <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="" />
+                  <h2 className=' whitespace-nowrap font-[font2] leading-[0.8] text-center pt-10 text-[8vw]
+                   uppercase'>pour tour voir</h2>
+                     <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c" alt="" />
+                
+                   </div>
+
+                   <div className=' moveX flex items-center'>
+                     <h2 className='whitespace-nowrap  font-[font2] leading-[0.8] text-center pt-10 text-[8vw] 
+                     uppercase'>pour tour voir</h2>
+                    <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="" />
+                  <h2 className=' whitespace-nowrap font-[font2] leading-[0.8] text-center pt-10 text-[8vw]
+                   uppercase'>pour tour voir</h2>
+                     <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c" alt="" />
+                
+                   </div>
+
+                </div>
+            </div>
+                   <div className='link relative border-t border-white'>
+
+                <h1 className=' font-[font2] leading-[0.8] text-center pt-10 text-[8vw] uppercase'>Projets</h1>
+                <div className='moveLink absolute flex top-0 bg-[#cfed52] text-black'>
+
+                   <div className=' moveX flex items-center'>
+                     <h2 className='whitespace-nowrap  font-[font2] leading-[0.8] text-center pt-10 text-[8vw] 
+                     uppercase'>pour tour voir</h2>
+                    <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="" />
+                  <h2 className=' whitespace-nowrap font-[font2] leading-[0.8] text-center pt-10 text-[8vw]
+                   uppercase'>pour tour voir</h2>
+                     <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c" alt="" />
+                
+                   </div>
+
+                   <div className=' moveX flex items-center'>
+                     <h2 className='whitespace-nowrap  font-[font2] leading-[0.8] text-center pt-10 text-[8vw] 
+                     uppercase'>pour tour voir</h2>
+                    <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="" />
+                  <h2 className=' whitespace-nowrap font-[font2] leading-[0.8] text-center pt-10 text-[8vw]
+                   uppercase'>pour tour voir</h2>
+                     <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c" alt="" />
+                
+                   </div>
+
+                </div>
+            </div>
+                   <div className='link relative border-t border-white'>
+
+                <h1 className=' font-[font2] leading-[0.8] text-center pt-10 text-[8vw] uppercase'>Projets</h1>
+                <div className='moveLink absolute flex top-0 bg-[#cfed52] text-black'>
+
+                   <div className=' moveX flex items-center'>
+                     <h2 className='whitespace-nowrap  font-[font2] leading-[0.8] text-center pt-10 text-[8vw] 
+                     uppercase'>pour tour voir</h2>
+                    <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="" />
+                  <h2 className=' whitespace-nowrap font-[font2] leading-[0.8] text-center pt-10 text-[8vw]
+                   uppercase'>pour tour voir</h2>
+                     <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c" alt="" />
+                
+                   </div>
+
+                   <div className=' moveX flex items-center'>
+                     <h2 className='whitespace-nowrap  font-[font2] leading-[0.8] text-center pt-10 text-[8vw] 
+                     uppercase'>pour tour voir</h2>
+                    <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="" />
+                  <h2 className=' whitespace-nowrap font-[font2] leading-[0.8] text-center pt-10 text-[8vw]
+                   uppercase'>pour tour voir</h2>
+                     <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c" alt="" />
+                
+                   </div>
+
+                </div>
+            </div>
+                   <div className='link relative border-y border-white'>
+
+                <h1 className=' font-[font2] leading-[0.8] text-center pt-10 text-[8vw] uppercase'>Projets</h1>
+                <div className='moveLink absolute flex top-0 bg-[#cfed52] text-black'>
+
+                   <div className=' moveX flex items-center'>
+                     <h2 className='whitespace-nowrap  font-[font2] leading-[0.8] text-center pt-10 text-[8vw] 
+                     uppercase'>pour tour voir</h2>
+                    <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="" />
+                  <h2 className=' whitespace-nowrap font-[font2] leading-[0.8] text-center pt-10 text-[8vw]
+                   uppercase'>pour tour voir</h2>
+                     <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c" alt="" />
+                
+                   </div>
+
+                   <div className=' moveX flex items-center'>
+                     <h2 className='whitespace-nowrap  font-[font2] leading-[0.8] text-center pt-10 text-[8vw] 
+                     uppercase'>pour tour voir</h2>
+                    <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="" />
+                  <h2 className=' whitespace-nowrap font-[font2] leading-[0.8] text-center pt-10 text-[8vw]
+                   uppercase'>pour tour voir</h2>
+                     <img className='h-28 rounded-full shrink-0 w-80 object-cover' src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c" alt="" />
+                
+                   </div>
+
+                </div>
+            </div>
+            
+            
+          
+        </div>
+    </div>
   )
 }
 
